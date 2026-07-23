4524eed976a783aa074957acdbabf0893caf1f08
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2026-07-23 00:01:14 -0400

    refactor

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import torch
+import os
 
 def create_vnnlib_str(data_lb: torch.Tensor, data_ub: torch.Tensor, prediction: torch.Tensor):
     # input bounds
@@ -66,3 +67,23 @@ def generate_property(vnnlib_path, data_lb, data_ub, n_class=2):
 
     with open(vnnlib_path, "w") as f:
         f.write(base_str)
+
+WEIGHT_PATH_DICT = {
+    "adult": "adult/AC-{}.nnet",
+    "compas": "compas/compas-{}.nnet",
+    "bank": "bank/BM-{}.nnet",
+    "german": "german/GC-{}.nnet",
+}
+
+NUM_DICT = {
+    "adult": 12,
+    "compas": 7,
+    "bank": 8,
+    "german": 5,
+}
+
+
+def get_weight_paths(args) -> list:
+    weight_path = os.path.join(args.model_dir, WEIGHT_PATH_DICT[args.dataset])
+    nums = NUM_DICT[args.dataset]
+    return [weight_path.format(idx) for idx in range(1, nums + 1)]
